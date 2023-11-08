5190d3a925abd6ae1ad2cac92f6afdb4b07aa41c
Kacper Lubczynski <kacperlubczynski564@gmail.com>
2023-11-08 20:12:05 +0000

    first commit

---
+++ ThermalSimulation.h
@@ -0,0 +1,15 @@
+//
+// Created by sudo on 11/8/23.
+//
+
+#ifndef CUBESATCPP_THERMALSIMULATION_H
+#define CUBESATCPP_THERMALSIMULATION_H
+
+
+class ThermalSimulation {
+private:
+
+};
+
+
+#endif //CUBESATCPP_THERMALSIMULATION_H

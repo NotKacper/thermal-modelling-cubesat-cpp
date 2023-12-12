1c4549014859ff5fece016cae7629c7d29a0ef38
Kacper Piotr Lubczynski <kacperlubczynski564@gmail.com>
2023-12-12 22:19:35 +0000

    Additional analysis script

---
+++ analysis.r
@@ -0,0 +1,20 @@
+thermalData <- read.csv("output-65.000000.csv")
+
+#Temperature <- matrix(
+#  thermalData$temperature,
+#  
+#  nrow = length(Time),
+#  ncol = length(BetaAngle),
+#  byrow = TRUE
+#)
+
+# persp3D(x=Time, y=BetaAngle, z=Temperature, theta=120)
+
+min(thermalData$temperature) - 273.15
+max(thermalData$temperature) - 273.15
+
+# Need to get Temperature readings into form A[i][j] = Temperature[time][beta_angle]
+
+# figure <- plot_ly() %>% add_surface(x=~Time, y=~BetaAngle, z=~Temperature)
+
+# figure
